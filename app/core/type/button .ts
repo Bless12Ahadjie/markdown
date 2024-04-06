2ce4e0e66f95cc920f3bef244eef9944cd1f44ff
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-04-06 09:09:24 +0000

    adding some files for core folder

---
+++ app/core/type/button .ts
@@ -0,0 +1,10 @@
+import { ReactElement, ReactSVGElement } from "react";
+
+export type btns = {
+    name?: string;
+    style?: React.CSSProperties;
+    className?: string;
+    src?: string;
+    alt?: string;
+    
+}
