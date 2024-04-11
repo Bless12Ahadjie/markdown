ef52a22af5c8aed3aae2a2336cff666e960736ac
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-04-11 17:35:08 +0000

    modification of the stores to delete based on index

--- app/core/libraries/forDocumentStore.ts
+++ app/core/libraries/forDocumentStore.ts
@@ -1,16 +1,19 @@
 import { makeObservable, observable, action, computed } from 'mobx';
+
 import { Document } from '../type/Types';
-import data from '../data/data.json'
+
+import data from '../data/data.json';
 
 class DocumentStore {
   documents: Document[] = [];
-
   currentDocumentId: number | null = null;
+  documentContent: string = ''; // Observable property to store the content
 
   constructor() {
     makeObservable(this, {
       documents: observable,
       currentDocumentId: observable,
+      documentContent: observable, // Make documentContent observable
       getCurrentDocumentName: computed,
       getCurrentId: computed,
       setCurrentDocumentId: action,
@@ -18,30 +21,33 @@ class DocumentStore {
       generateId: action,
       updateDocumentName: action,
       deleteDocument: action,
-      updateDocumentContent: action
+      updateDocumentContent: action,
     });
 
     this.documents = data;
-
+    this.documents = data;
+    this.currentDocumentId = 2; 
+    this.documentContent = this.documents.find((doc) => doc.id === 2)?.content || ''; 
   }
 
   get getCurrentDocumentName() {
     const document = this.documents.find(doc => doc.id === this.currentDocumentId);
     return document?.name ?? 'welcome.md';
   }
- 
-  get getCurrentId(){
+
+  get getCurrentId() {
     const document = this.documents.find(doc => doc.id === this.currentDocumentId);
     return document?.id ?? 2;
   }
+
   setCurrentDocumentId(docId: number) {
     this.currentDocumentId = docId;
-    
+    this.documentContent = (this.documents.find(doc => doc.id === docId)?.content) || '';
   }
 
   generateId = () => {
     return Math.floor(Math.random() * 10000);
-  }
+  };
 
   addDocument = () => {
     const newDocumentId = this.generateId();
@@ -62,9 +68,10 @@ class DocumentStore {
     };
 
     this.currentDocumentId = newDocumentId;
-    this.documents = [newDocument, ...this.documents]
+    this.documents = [newDocument, ...this.documents];
+    this.documentContent = '';
     return newDocumentId;
-  }
+  };
 
   updateDocumentName = (newName: string) => {
     const document = this.documents.find(doc => doc.id === this.currentDocumentId);
@@ -73,16 +80,19 @@ class DocumentStore {
     }
   };
 
-  deleteDocument = () => {
-    this.documents = this.documents.filter(doc => doc.id !== 1 && doc.id !== this.currentDocumentId);
+  deleteDocument = (id?: number | null) => {
+    this.documents = this.documents.filter(doc => doc.id !== this.currentDocumentId);
+    this.currentDocumentId = null;
+    this.documentContent = '';
   };
 
-  updateDocumentContent(content: string) {
-    const document = this.documents.find(doc => doc.id === this.currentDocumentId);
+  updateDocumentContent = (content: string) => {
+    const document: Document | undefined = this.documents.find(doc => doc.id === this.currentDocumentId);
     if (document) {
       document.content = content;
+      this.documentContent = content;
     }
-  }
+  };
 }
 
 const documentStore = new DocumentStore();
